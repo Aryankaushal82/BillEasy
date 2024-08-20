9ce57a2e6730de8ffabe5ee5389699c75cfdb128
aryan 138 <apathania206@gmail.com>
2024-08-20 07:10:27 +0530

    features name in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import HeroPage from "./component/LandingPage/HeroSection/HeroPage.jsx";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './component/LandingPage/Features/navbar.jsx';
-import Gstin from './component/LandingPage/Features/GSTIN.jsx';
-import Hsnsaccodes from './component/LandingPage/Features/HSN-SAC CODES.jsx';
-import Invoices from './component/LandingPage/Features/INVOICES.jsx';
-import Taxes from './component/LandingPage/Features/TAXES.jsx';
-import EWAYBILLS from './component/LandingPage/Features/E-WAY BILLS.jsx';
-import DELIVERYCHALLAN from './component/LandingPage/Features/DELIVERY CHALLAN.jsx';
+import Navbar from './component/LandingPage/features/navbar.jsx';
+import Gstin from './component/LandingPage/features/GSTIN.jsx';
+import Hsnsaccodes from './component/LandingPage/features/HSN-SAC CODES.jsx';
+import Invoices from './component/LandingPage/features/INVOICES.jsx';
+import Taxes from './component/LandingPage/features/TAXES.jsx';
+import EWAYBILLS from './component/LandingPage/features/E-WAY BILLS.jsx';
+import DELIVERYCHALLAN from './component/LandingPage/features/DELIVERY CHALLAN.jsx';
 
 function App() {
   return (
