d051f4b3ba9c586da27b75cf0706672b1ced7951
aryan 138 <apathania206@gmail.com>
2024-08-21 14:09:00 +0530

    updated Backend

---
+++ Backend/middlewares/dB.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { moncrd } = require('../config/cred')
+
+const url = `mongodb+srv://${moncrd.user}:${moncrd.pass}@cluster0.oxhj6pg.mongodb.net`;
+
+
+const dbConnect = async () => {
+  try {
+    const data = await mongoose.connect(url)
+    console.log('Db Connected ')
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = dbConnect;
