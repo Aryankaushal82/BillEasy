a3cc9171ee6dd47605a19df5454d7d80708537e4
aryan 138 <apathania206@gmail.com>
2024-08-30 02:46:25 +0530

    updated routes and removed vulnerabilities

--- Backend/models/customer.js
+++ Backend/models/customer.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const customerSchema = new mongoose.Schema({
-    customer_id : { type: String, required: true, unique: true },
-    customer_name : { type: String },
-    customer_email : { type: String, required: true },
-    branch_id : { type: String },
-    branch_email : { type: String , required: true}
-})
+    customer_id: { type: String, required: true, unique: true },
+    customer_name: { type: String },
+    customer_email: { type: String, required: true },
+    branch: { type: mongoose.Schema.Types.ObjectId, ref: 'admin' },
+}, { timestamps: true })
 
 module.exports = mongoose.model('customer', customerSchema)
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    user_id : { type: String, required: true, unique: true },
-    user_name : { type: String, required: true },
-    user_email : { type: String, required: true, unique: true },
-    user_role : { type: String },
-    user_status : { type: String },
-    user_assigned_branch : { type:  String}
-})
+    user_id: { type: String, required: true, unique: true },
+    user_name: { type: String, required: true },
+    user_email: { type: String, required: true, unique: true },
+    user_role: { type: String },
+    user_status: { type: String },
+    user_assigned_branch: { type: String }
+}, { timestamps: true })
 
 module.exports = mongoose.model('user', userSchema)
